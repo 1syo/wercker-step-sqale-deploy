bb4960d7c45ac1233e4a8cca61f14b59c416e787
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-09-27 21:26:03 +0900

    Update run.sh

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+if [ ! "$WERCKER" = true ]; then
+  fail "Outside the wercker environment, not deploy."
+fi
+
+if [ ! -d "$HOME/.ssh" ]; then
+  mkdir -p $HOME/.ssh
+fi
+
+private_key_path=`mktemp`
+echo $WERCKER_SQALE_PRIVATE_KEY > ${private_key_path}
+info "Set up the private key."
+
+cat <<-__CONFIG__ > $HOME/.ssh/config
+  Host gateway_sqale_jp
+    User sqale
+    Hostname gateway.sqale.jp
+    Port 2222
+    UserKnownHostsFile /dev/null
+    StrictHostKeyChecking no
+    IdentityFile ${private_key_path}
+__CONFIG__
+chmod 600 $HOME/.ssh/config
+info "Set up the ssh config."
+
+now=`date +%s`
+rm -rf .bundle
+git remote add sqale ssh://gateway_sqale_jp$WERCKER_SQALE_REPOSITORY
+git checkout -b ${now}
+git push sqale ${now}:master
+success "Deployment to sqale finished successfully!"
